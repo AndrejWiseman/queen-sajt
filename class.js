8f5732120158c9cc2be0d6fc468e49a44229ec79
A.M <andrej2000aa@gmail.com>
2022-10-01 14:57:10 +0200

    prvi push

---
+++ class.js
@@ -0,0 +1,159 @@
+'use strict';
+
+class MyNav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <nav>
+            <div class="hamb-ram"></div>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+            
+            <ul class="list-items">
+                <li class="item"><a href="index.html">Početna</a></li>
+                <li class="item"><a href="#s2">O nama</a></li>
+                <li class="item"><a href="#s3">Galerija</a></li>
+                <li class="item"><a href="#s1">Lokacija</a></li>
+                <li class="item"><a href="rezervacija.html">Kontakt</a></li>
+            </ul>
+            
+            <div class="logo2">
+                <a href="index.html"><img src="img/logo3.png" alt="logo"></a>
+            </div>
+            
+            <div class="buk-ram"></div>
+            <div class="booking">
+                <button class="book-dugme" onclick="location.href = 'rezervacija.html';">Rezerviši termin</button>
+            </div>
+        </nav>
+    `
+    }
+}
+customElements.define('my-nav', MyNav);
+
+
+
+
+
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <div class="fbox1">
+                <div class="fbox1-1">
+                    <h3 class="fh3">Salon</h3>
+                    <ul>
+                        <li class="f-li"><a href="#">O nama</a></li>
+                        <li class="f-li"><a href="#">Galerija</a></li>
+                        <li class="f-li"><a href="#">Usluge</a></li>
+                    </ul>
+                </div>
+                
+                <div class="fbox1-2">
+                    <h3 class="fh3">Radno vrijeme</h3>
+                    <table>
+                        <tr>
+                            <td>Ponedjeljak</td>
+                            <td>9.00 - 20.00</td>
+                        </tr>
+                        <tr>
+                            <td>Utorak</td>
+                            <td>9.00 - 20.00</td>
+                        </tr>
+                        <tr>
+                            <td>Srijeda</td>
+                            <td>9.00 - 20.00</td>
+                        </tr>
+                        <tr>
+                            <td>Četvrtak</td>
+                            <td>12.00 - 20.00</td>
+                        </tr>
+                        <tr>
+                            <td>Petak</td>
+                            <td>12.00 - 20.00</td>
+                        </tr>
+                        <tr>
+                            <td>Subota</td>
+                            <td>9.00 - 14.00</td>
+                        </tr>
+                        <tr>
+                            <td>Nedjelja</td>
+                            <td>Ne radimo</td>
+                        </tr>
+                    </table>
+                </div>
+                
+                <div class="fbox1-3">
+                    <h3 class="fh3">Kontakt</h3>
+                    <div class="adresa">
+                        <p class="adr1">Adresa:</p>
+                        <p class="adr2">Vladike Platona 26c, 78430 Prnjavor <br> Bosna i Hercegovina</p>
+                        <p class="adr3"><a href="https://goo.gl/maps/bs5eUjS4Lek41Pzm9" target="_blank">Lokacija salona</a></p>
+                    </div>
+                    <div class="telefon">
+                        <p class="tel1">Telefon:</p>
+                        <p class="tel2">+387 65 255 827</p>
+                    </div>
+                    <div class="social">
+                        <a href="https://www.instagram.com/kozmeticki_salon_queen/">                        <i class="fa-brands fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/ks.queen.prnjavor">                        <i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="https://m.me/Kozmetički salon - QUEEN=fm"><i class="fa-brands fa-facebook-messenger"></i></a>
+                        <a href="viber://add?number=+38765255827"><i class="fa-brands fa-viber"></i></a>
+                        <a href="tel: +38765255827"><i class="fa-solid fa-phone"></i></a>
+                    </div>
+                </div>
+
+            </div>
+            
+            <div class="fbox2">
+                <p>&copy 2022 Queen Prnjavor</p>
+            </div>        
+        </footer>
+    `
+    }
+}
+customElements.define('my-footer', MyFooter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,1 +1,146 @@
-// JavaScript Document
+'use strict';
+
+//===== navigacija =======================
+let hamburger = document.querySelector('.hamburger');
+let meni = document.querySelector('.list-items');
+let link = document.querySelectorAll('.item');
+let bar = document.querySelectorAll('.bar');
+let bukDugme = document.querySelector('.book-dugme');
+
+let hamRam = document.querySelector('.hamb-ram');
+let bukRam = document.querySelector('.buk-ram');
+
+function otvoriMeni() {
+    meni.classList.toggle('pojavljivanje');
+    hamburger.classList.toggle('ham-back');
+    bar.forEach(item => item.classList.toggle('bar-back'))
+    window.scrollTo(0, 0);
+}
+
+function zatvoriMeni() {
+    meni.classList.toggle('pojavljivanje');
+    hamburger.classList.toggle('ham-back');
+    bar.forEach(item => item.classList.toggle('bar-back'))
+}
+
+link.forEach(item => item.addEventListener('click', zatvoriMeni))
+
+hamburger.addEventListener('click', otvoriMeni);
+
+hamburger.addEventListener('mouseenter', function(){
+    hamRam.classList.add('hidden');
+    hamburger.classList.add('ham-out')
+})
+hamburger.addEventListener('mouseleave', function(){
+    hamRam.classList.remove('hidden');
+    hamburger.classList.remove('ham-out')
+})
+bukDugme.addEventListener('mouseenter', function(){
+    bukRam.classList.add('hidden');
+    bukDugme.classList.add('ham-out')
+})
+bukDugme.addEventListener('mouseleave', function(){
+    bukRam.classList.remove('hidden');
+    bukDugme.classList.remove('ham-out');
+})
+//=========================================
+
+//== loader ================
+let body = document.querySelector('body');
+
+body.onload = function() {myFunction()}
+
+let myVar;
+let quLogo;
+function myFunction() {
+    myVar = setTimeout(showPage, 500);
+    quLogo = setTimeout(logoShow, 1100);
+    headBg = setTimeout(hBg, 900);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+function logoShow() {
+    document.querySelector(".logo").style.opacity = "1";
+}
+function hBg() {
+//    document.querySelector("header").style.backgroundSize = "cover";
+    document.querySelector(".head-box").style.transform =  "scale(1)";
+}
+
+//============================
+var rellax = new Rellax('.rellax', {
+    center: true,
+//    wrapper: 'header'
+});
+//============================
+
+
+// pojavljivanje elemenata na scroll =============
+const allText = document.querySelectorAll('.scroll-pojava');
+
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+
+    if(!entry.isIntersecting) return;
+
+    entry.target.classList.remove('reveal')
+
+    observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver
+(revealSection, {
+    root: null,
+    threshold: 0.2
+})
+
+allText.forEach(function (text) {
+    sectionObserver.observe(text);
+    text.classList.add('reveal');
+});
+//==============================================
+
+
+
+//=== carousel glavni ====================
+//let slideIndex = 0;
+//showSlides();
+//
+//function showSlides() {
+//  let i;
+//  let slides = document.getElementsByClassName("mySlides");
+//  let dots = document.getElementsByClassName("dot");
+//  for (i = 0; i < slides.length; i++) {
+//    slides[i].style.display = "none";  
+//  }
+//  slideIndex++;
+//  if (slideIndex > slides.length) {slideIndex = 1}    
+//  for (i = 0; i < dots.length; i++) {
+//    dots[i].className = dots[i].className.replace(" active", "");
+//  }
+//  slides[slideIndex-1].style.display = "block";  
+//  dots[slideIndex-1].className += " active";
+//  setTimeout(showSlides, 3000); // Change image every 2 seconds
+//}
+
+//======================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
